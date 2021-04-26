53a6e759087e79daee3e86b7e9cc3b6894f0dcff
Jack Petersen <peterja7@os2.engr.oregonstate.edu>
2021-04-25 17:21:34 -0700

    leap year without error checking

---
+++ leapYear.py
@@ -0,0 +1,17 @@
+quit1 = 0
+while quit1 == 0:
+    year = input("Enter a year: ")
+    year = int(year)
+    if  year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                print(year, "is a leap year\n")
+            else:
+                print(year, "is not a leap year\n")
+        else:
+            print(year, "is a leap year\n")
+        
+    else:
+        print(year, "is not a leap year\n")
+    quit1 = input("To enter another year press 0, to quit press 1: ")
+    quit1 = int(quit1)
